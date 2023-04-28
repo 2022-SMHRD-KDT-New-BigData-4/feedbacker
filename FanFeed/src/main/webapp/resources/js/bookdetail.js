e2e373c0064eade9b5aac7106a167c930790380c
smhrd <smhrd@DESKTOP-1JRSK37>
2023-04-28 12:07:08 +0900

    123

---
+++ FanFeed/src/main/webapp/resources/js/bookdetail.js
@@ -0,0 +1,21 @@
+/** 북 **/
+
+
+   $(document).ready(function () {
+		var isbn = $('.isbn').text()
+       $.ajax({
+           url : "flask.do",
+           type : "post",
+           data : {isbn:isbn},
+           dataType : "json",
+           success : function (data) {
+               console.log(data);
+               $.each(data.predict.lda, function (index, word) {
+               $.each(word, function (key, value) {
+                  console.log("Key: " + key + ", Value: " + value)
+               })
+            })
+           },
+           error : function () { alert("에러"); }
+       });
+   });
